c793436b6bcdbbcbe2b67cf596706b353a78f0c1
Jonathan Chhabra <Sonicrida@users.noreply.github.com>
2017-07-30 00:01:13 -0400

    Add routing and menu

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo';
+import { BrowserRouter } from 'react-router-dom';
+
 
 const networkInterface = createNetworkInterface({
     uri: process.env.REACT_APP_GRAPHQL_ENDPOINT
@@ -14,9 +16,11 @@ const client = new ApolloClient({
 });
 
 ReactDOM.render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>, 
-document.getElementById('root')
+    <BrowserRouter>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
 registerServiceWorker();
